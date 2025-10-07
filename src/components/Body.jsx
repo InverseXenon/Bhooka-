fdc2c3f2d186402d1729fbcc5fc94eaeaa3f1dc4
Piyush Patil <piyushpatil1741@gmail.com>
2025-10-07 23:55:10 +0530

    Added Functionality to Search bar(but is onClick and not onChange.)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,41 @@
     import restaurantData from "../mockData/mockdata";
     import RestaurantCard from "./RestaurantCard";
+    import { useState } from "react";
+
+    function filterData(query, hotels){
+        return hotels.filter((hotels)=>
+             hotels.name.includes(query)
+
+
+        )
+    }
+
     function Body(){
+        const [hotels, setHotels] = useState(restaurantData);
+        const [query, setQuery] = useState("");
+
         return(
             <>
+            
                 <div className="search">
-                    <input type="text" className="search-input" placeholder="Search Hotel/Food" />
-                    <button className="search-btn"><img src="https://static.thenounproject.com/png/4009566-200.png" alt="Search" className="search-icon" /></button>
+                    
+                    <input type="text"
+                     className="search-input" 
+                     placeholder="Search Hotel/Food" 
+                     value={query}
+                     onChange={(e) =>{
+                        setQuery(e.target.value);
+                     }}/>
+                    <button className="search-btn" 
+                     onClick={()=>{
+                        const data = filterData(query,hotels);
+                        setHotels(data);
+                     }}
+                    ><img src="https://static.thenounproject.com/png/4009566-200.png" alt="Search" className="search-icon" /></button>
                     
                 </div>
                 <div className="restaurant-list">
-                    {restaurantData.map((res,index)=>(
+                    {hotels.map((res,index)=>(
                         <RestaurantCard 
                             key={index}
                             image={res.image}
