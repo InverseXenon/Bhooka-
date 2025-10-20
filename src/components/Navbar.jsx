2ee863bc6a73c1ab364a8412a86fe64d01d72354
Piyush Patil <piyushpatil1741@gmail.com>
2025-10-20 23:53:40 +0530

    Minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,6 @@ function Navbar(){
                 <div className="flex py-10 justify-between">
                     <Link to="/"><button className="px-4 py-2 mx-1 bg-blue-700 text-white rounded hover:bg-blue-800 transition">Home</button></Link>                    
                     <Link to="/about"><button className="px-4 py-2 mx-1 bg-blue-700 text-white rounded hover:bg-blue-800 transition">About Us</button></Link>
-                    <Link to="/about"><button className="px-4 py-2 mx-1 bg-blue-700 text-white rounded hover:bg-blue-800 transition">About Us</button></Link>
                     <Link to="/contact"><button className="px-4 py-2 mx-1 bg-blue-700 text-white rounded hover:bg-blue-800 transition">Contact Us</button></Link>
                     <Link to="/instamangao"><button className="px-4 py-2 mx-1 bg-blue-700 text-white rounded hover:bg-blue-800 transition">InstaMangao</button></Link>
                     <button className="px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-800 transition">Cart</button>
